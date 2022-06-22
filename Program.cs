d4cb18d5e2e7a9ed3a5fc9d6cab2e65491f441b7
Sam Wang <samofcorinth@gmx.ca>
2022-06-22 00:38:30 -0700

    Configured HttpClient

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using NaidiraClient;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("APIClient", client => client.BaseAddress = builder.HostEnvironment.IsDevelopment() ?
+   new Uri("http://localhost:3000") : new Uri("https://naidira.api.5am.ooo"));
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("APIClient"));
 
 await builder.Build().RunAsync();
