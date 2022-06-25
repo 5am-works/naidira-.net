ebc7bda3017d7bc9684117517b9094b6621067bc
Sam Wang <samofcorinth@gmx.ca>
2022-06-25 03:35:08 -0700

    Added alphabetical list page

--- Data/SearchResults.cs
+++ Data/SearchResults.cs
@@ -3,10 +3,10 @@
 namespace NaidiraClient.Data; 
 
 public class SearchResults {
-   [JsonPropertyName("word_results")] public List<WordResult> WordResults { get; init; } = null!;
+   [JsonPropertyName("word_results")] public List<DictionaryEntry> WordResults { get; init; } = null!;
 }
 
-public class WordResult {
+public class DictionaryEntry {
    [JsonPropertyName("spelling")] public string Spelling { get; set; } = "";
    [JsonPropertyName("word_type")] public string WordType { get; set; } = "";
    [JsonPropertyName("meaning")] public string Meaning { get; set; } = "";
---
+++ Data/Word.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NaidiraClient.Data;
+
+public class Word {
+   [JsonPropertyName("type")] public string Type { get; set; } = "";
+   [JsonPropertyName("spelling")] public string Spelling { get; set; } = "";
+   [JsonPropertyName("simple_meaning")] public string SimpleMeaning { get; set; } = "";
+   [JsonPropertyName("modifiable_types")] public List<string>? ModifiableTypes { get; set; }
+   [JsonPropertyName("attachment_types")] public List<string>? AttachmentTypes { get; set; }
+   [JsonPropertyName("attachment_notes")] public List<string?>? AttachmentNotes { get; set; }
+   [JsonPropertyName("formatted_meaning")] public string? FormattedMeaning { get; set; }
+}
