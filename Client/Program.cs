0b01ad7b0e52089c7c0ab28d9bce0bcab5b88876
Sam Wang <samofcorinth@gmx.ca>
2022-09-18 00:56:53 -0700

    Update GitHub Actions

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddHttpClient("APIClient", client => client.BaseAddress = builder.HostEnvironment.IsDevelopment() ?
-   new Uri("http://localhost:7071/") : new Uri("https://naidira.api.5am.ooo"));
+   new Uri("http://localhost:7071/") : new Uri("https://naidira-api.azurewebsites.net"));
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("APIClient"));
 
 await builder.Build().RunAsync();
