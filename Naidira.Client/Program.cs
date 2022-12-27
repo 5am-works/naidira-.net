7b91bba9b47394fa2a4dfa0efafeb0c99ba6f175
Sam Wang <samofcorinth@gmx.ca>
2022-12-27 14:59:11 -0800

    Updated client base address

--- Naidira.Client/Program.cs
+++ Naidira.Client/Program.cs
@@ -5,9 +5,12 @@ using Naidira.Client;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddHttpClient("APIClient", client => client.BaseAddress = builder.HostEnvironment.IsDevelopment() ?
-   new Uri("http://localhost:7071/") : new Uri("https://naidira-api.azurewebsites.net"));
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("APIClient"));
+builder.Services.AddHttpClient("APIClient", client => client.BaseAddress =
+   new Uri(builder.HostEnvironment.IsDevelopment()
+      ? "http://localhost:7071/"
+      : builder.HostEnvironment.BaseAddress));
+builder.Services.AddScoped(sp =>
+   sp.GetRequiredService<IHttpClientFactory>().CreateClient("APIClient"));
 var host = builder.Build();
 host.Services.GetRequiredService<ILoggerFactory>()
    .CreateLogger<Program>();
